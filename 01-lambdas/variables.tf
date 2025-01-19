2cb9aeff49ae1f76c3d48b18c9e546507ecc96ff
Mike Monaco <mamonaco1973@gmail.com>
2025-01-19 12:47:14 -0500

    clean up variables.tf

--- 01-lambdas/variables.tf
+++ 01-lambdas/variables.tf
@@ -1,8 +1,6 @@
-# Boolean Variable for Anonymous Access
-# Determines whether to enable anonymous access to the deployed Cloud Functions (public invocation).
 variable "authorization_type" {
   description = "What type of authorization to require on the routes"
-  type        =  string  # Data type of the variable (boolean).
+  type        =  string
 #  default     =  "AWS_IAM"
   default     = "NONE" 
 }
